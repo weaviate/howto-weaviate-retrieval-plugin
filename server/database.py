0a54b02f48e3a7d0d7136a7538786fc265b81d3e
hsm207 <hsm207@users.noreply.github.com>
2023-04-14 13:49:30 +0000

    implement database init functions
    Signed-off-by: hsm207 <hsm207@users.noreply.github.com>

---
+++ server/database.py
@@ -0,0 +1,34 @@
+import weaviate
+import os
+import logging
+
+SCHEMA = {
+    "class": "Document",
+    "properties": [
+        {"name": "text", "dataType": ["text"]},
+        {"name": "document_id", "dataType": ["string"]},
+    ],
+}
+
+
+def get_client():
+    """
+    Get a client to the Weaviate server
+    """
+    host = os.environ.get("WEAVIATE_HOST", "http://localhost:8080")
+    return weaviate.Client(host)
+
+
+def init_db():
+    """
+    Create the schema for the database if it doesn't exist yet
+    """
+    client = get_client()
+
+    if not client.schema.contains(SCHEMA):
+        logging.debug("Creating schema")
+        client.schema.create_class(SCHEMA)
+    else:
+        class_name = SCHEMA["class"]
+        logging.debug(f"Schema for {class_name} already exists")
+        logging.debug("Skipping schema creation")
---
+++ tests/server/test_database.py
@@ -0,0 +1,16 @@
+from server import database
+import logging
+
+def test_init_db(caplog, weaviate_client):
+    """
+    Test the database initialization
+    """
+    with caplog.at_level(logging.DEBUG):
+        database.init_db()
+        assert "Creating schema" in caplog.text
+    
+    caplog.clear()
+    
+    with caplog.at_level(logging.DEBUG):
+        database.init_db()
+        assert "Skipping schema creation" in caplog.text
